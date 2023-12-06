7cced328d50d0daf1fd7d9ede74c27eb94424028
Arthur Mendes <arthurmco@gmail.com>
2023-12-06 03:29:53 -0300

    day6: solve day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -14,6 +14,13 @@ fn parse_races(time_line: &str, distance_line: &str) -> Vec<Race> {
     times.zip(distances).map(|(t, d)| (t, d)).collect()
 }
 
+fn parse_races2(time_line: &str, distance_line: &str) -> Vec<Race> {
+    let times = split_numbers_by_space(&time_line[9..].replace(" ", "")).into_iter();
+    let distances = split_numbers_by_space(&distance_line[9..].replace(" ", "")).into_iter();
+    
+    times.zip(distances).map(|(t, d)| (t, d)).collect()
+}
+
 fn distance_for_button_held(race_time: Milisecond, button_held_for_ms: Milisecond) -> Milimeter {
     let remaining = race_time.saturating_sub(button_held_for_ms);
 
@@ -28,14 +35,32 @@ fn victories_for_each_record(race: Race) -> Vec<Milimeter> {
     (0..=time).map(|n| distance_for_button_held(time, n)).filter(|&n| n > record_distance).collect()
 }
 
-
-pub fn day6() {
+/*
+pub fn day6p1() {
     let game_file = read_file_as_text("./inputs/day6real.txt");
     //let game_file = read_file_as_text("./inputs/day6test.txt");
 
     let mut lines = game_file.lines().take(2);
     let races = parse_races(&lines.next().unwrap().unwrap(),
                             &lines.next().unwrap().unwrap());
+    
+    println!("Hello {:?}", races);
+    let victories: usize = races.iter().map(|r| {
+        let vs = victories_for_each_record(r.clone());
+        vs.len()
+    }).inspect(|v| println!("{}", v)).product();
+
+    println!("\n{}", victories);
+}
+*/
+
+pub fn day6() {
+    let game_file = read_file_as_text("./inputs/day6real.txt");
+    //let game_file = read_file_as_text("./inputs/day6test.txt");
+
+    let mut lines = game_file.lines().take(2);
+    let races = parse_races2(&lines.next().unwrap().unwrap(),
+                             &lines.next().unwrap().unwrap());
     
     println!("Hello {:?}", races);
     let victories: usize = races.iter().map(|r| {
